4c6ddde789a6eeb514e0bfdeacb19c342abd244c
DevCesar <72570596+DevCesarr@users.noreply.github.com>
2025-03-05 07:36:22 -0300

    Update usuario.blade.php

--- usuario.blade.php
+++ usuario.blade.php
@@ -3,7 +3,7 @@
 <head><script src="https://getbootstrap.com/docs/5.3/assets/js/color-modes.js"></script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="style.css" rel="stylesheet">
+  <link rel="styleusuario.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
   <title>Login</title>
