ecf72b8de531a79690f18522cd10174a89a7cfba
DevCesarr <cesarhsduarte@gmail.com>
2025-03-09 22:53:58 -0300

    aaa

--- storage/framework/views/53f3e9b29301bbbe86a5d0a1d4d49c82.php
+++ storage/framework/views/53f3e9b29301bbbe86a5d0a1d4d49c82.php
@@ -10,7 +10,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
+
     <!-- Seu CSS personalizado -->
     <link rel="stylesheet" href="<?php echo e(asset('css/styles.css')); ?>">
 
@@ -18,7 +18,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
-    
+
     <?php echo $__env->yieldContent('head'); ?> <!-- Permite incluir algo extra no <head> se necessário -->
 
 </head>
@@ -46,7 +46,7 @@
 
         <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-                <a class="nav-link btn-menu" id="btn-login" href="#">Entrar</a>
+                <a class="nav-link btn-menu" id="btn-login" href="login">Entrar</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link btn-menu" id="btn-parceiro" href="#">Seja um parceiro</a>
