8b3e4975880215c7f9963a57e2927de753a80808
relapso <v.i.n2@icloud.com>
2025-11-16 19:33:10 -0300

    Fix: import Artisan facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
