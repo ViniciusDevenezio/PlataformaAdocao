bb15bc96497c07ce64cb927d14322a7422f39ea1
Vini Dev <viniciusdevenezio@gmail.com>
2025-11-16 17:08:15 -0300

    Enforce HTTPS handling and secure asset URLs

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * Using "*" trusts all proxies so HTTPS headers like X-Forwarded-Proto
+     * are honored in load-balanced environments. Adjust as needed.
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -154,9 +154,9 @@
 {{-- scripts iguais aos do login --}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-<script src="{{ asset('js/validarCpf.js') }}"></script>
-<script src="{{ asset('js/cepAutoComplete.js') }}"></script>
-<script src="{{ asset('js/cidadesEstados.js') }}"></script>
+<script src="{{ secure_asset('js/validarCpf.js') }}"></script>
+<script src="{{ secure_asset('js/cepAutoComplete.js') }}"></script>
+<script src="{{ secure_asset('js/cidadesEstados.js') }}"></script>
 
 <script>
     $(function () {
